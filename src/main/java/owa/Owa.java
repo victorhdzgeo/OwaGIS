15a997351d139aabc1b30fe5de6c659d6b1f623e
Víctor Hernández <victorhdzgeo@gmail.com>
2023-11-29 19:33:13 -0600

    Cambio de nombre en el package

--- src/main/java/owa/Owa.java
+++ src/main/java/owa/Owa.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
 
-package pa.owa;
+package owa;
 import java.io.File;
 import java.io.IOException;
 import utils.FuncionValor;
